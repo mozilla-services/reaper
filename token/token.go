26861e9684595c490b516e9d97c00e140987a02c
Benson Wong <mostlygeek+git@gmail.com>
2015-03-17 16:39:44 -0700

    remove old scrypt code

--- token/token.go
+++ token/token.go
@@ -11,20 +11,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"sort"
 	"strings"
 	"time"
-
-	"golang.org/x/crypto/scrypt"
 )
 
 const (
-	SALT_SIZE  = 16
-	SCRYPT_N   = 16384
-	SCRYPT_R   = 8
-	SCRYPT_P   = 2
-	SCRYPT_LEN = 32
-	DELIMIT    = "|"
+	DELIMIT = "|"
 )
 
 type JobToken struct {
@@ -149,33 +141,4 @@ func Untokenize(key []byte, t string) (j *JobToken, err error) {
 	}
 
 	return
-}
-
-func createPayload(m map[string]string) []string {
-	// add values to the hmac in sorted order
-	var keys []string
-	var vals []string
-	for k := range m {
-		keys = append(keys, k)
-	}
-
-	sort.Strings(keys)
-	for _, k := range keys {
-		vals = append(vals, k+":"+m[k])
-	}
-
-	return vals
-}
-
-func CreateToken(secret, salt []byte, vars map[string]string) []byte {
-	sKey, _ := scrypt.Key(secret, salt, SCRYPT_N, SCRYPT_R, SCRYPT_P, SCRYPT_LEN)
-	mac := hmac.New(sha256.New, sKey)
-	mac.Write(salt)
-
-	for _, v := range createPayload(vars) {
-		mac.Write([]byte(v))
-
-	}
-
-	return mac.Sum(nil)
 }

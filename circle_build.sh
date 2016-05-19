7af0d1446feb2c5d31f4a3140efec98481aa00e7
Jeremy Orem <jeremy.orem@gmail.com>
2016-05-19 15:38:27 -0700

    quote IMPORT_PATH

--- circle_build.sh
+++ circle_build.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./bin
 
-exec docker run -it -v "$PWD:/go/src/$IMPORT_PATH" -v "$PWD/bin:/go/bin" golang:1.6 go install  --ldflags '-extldflags "-static"' $IMPORT_PATH
+exec docker run -it -v "$PWD:/go/src/$IMPORT_PATH" -v "$PWD/bin:/go/bin" golang:1.6 go install  --ldflags '-extldflags "-static"' "$IMPORT_PATH"
